a46b63aba11a90740bb2d0524b9aeaaf368bb730
wamiq <wamiqidreesia381z@gmail.com>
2018-12-13 18:45:29 +0500

    .net project
    .net project

---
+++ WebApplication8/WebApplication8/Controllers/homeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class homeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(user u)
+        {
+            return View();
+        }
+    }
+}
